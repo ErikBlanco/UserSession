15ba0928e8383e75a682343a74d131886db400e4
Erik Blanco <erikblancog@hotmail.com>
2024-01-18 12:25:53 -0800

    Register and Sign In controls

--- UserSession/Controllers/RegisterViewController.swift
+++ UserSession/Controllers/RegisterViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func registerTapped(_ sender: UIButton) {
+        
+    }
 }
 
